f2abdcb733c0f8a4bb59ad3525272afee3b5697d
Maxime Leduc <maxime@aldebaran.care>
2025-10-10 00:00:29 +0200

    upgrade

--- next.config.js
+++ next.config.js
@@ -14,13 +14,13 @@ module.exports = {
   env: {
     nextImageExportOptimizer_imageFolderPath: "public/images",
     nextImageExportOptimizer_exportFolderPath: "out",
-    nextImageExportOptimizer_quality: 75,
-    nextImageExportOptimizer_storePicturesInWEBP: false,
+    nextImageExportOptimizer_quality: "75",
+    nextImageExportOptimizer_storePicturesInWEBP: "false",
     nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
 
     // If you do not want to use blurry placeholder images, then you can set
     // nextImageExportOptimizer_generateAndUseBlurImages to false and pass
     // `placeholder="empty"` to all <ExportedImage> components.
-    nextImageExportOptimizer_generateAndUseBlurImages: true,
+    nextImageExportOptimizer_generateAndUseBlurImages: "true",
   },
 };
